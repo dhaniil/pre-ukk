b857cb5ac817c6b0924c8a1ebf7d66b694d75874
HandlerOne <pemudakrinj20394@gmail.com>
2025-06-01 19:08:22 +0700

    Refactor RoleSeeder: Improve role creation logic and enhance permission assignment for teacher and student roles.

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 
@@ -16,12 +17,10 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        // Admin Data
         $name = 'Admin';
         $email = 'admin@ilazer.me';
         $password = '12345678';
-
-        // Create Role
+        
         $user = User::create([
             'name' => $name,
             'email_verified_at' => Carbon::now(),
@@ -30,11 +29,31 @@ class RoleSeeder extends Seeder
         ]);
 
         $superAdminRole = Role::create(['name' => 'super_admin', 'guard_name' => 'web']);
-        Role::create(['name' => 'teacher', 'guard_name' => 'web']);
-        Role::create(['name' => 'student', 'guard_name' => 'web']);
-        Role::create(['name' => 'student', 'guard_name' => 'web']);
+        $teacherRole = Role::create(['name' => 'teacher', 'guard_name' => 'web']);
+        $studentRole = Role::create(['name' => 'student', 'guard_name' => 'web']);
+
+        Artisan::call('shield:generate', ['--all' => true, '--panel' => 'admin']);
 
         $superAdminRole->givePermissionTo(\Spatie\Permission\Models\Permission::all());
         $user->assignRole($superAdminRole);
+
+        $teacherPermissions = [
+            'view_any_industries',
+            'view_industries',
+            'view_any_internship', 
+            'view_internship',
+            'view_any_student',
+            'view_student',
+        ];
+        $teacherRole->givePermissionTo($teacherPermissions);
+
+        $studentPermissions = [
+            'view_any_industries', 
+            'view_industries', 
+            'view_internship',    
+            'update_internship',   
+            'page_StudentDashboard',
+        ];
+        $studentRole->givePermissionTo($studentPermissions);
     }
 }
